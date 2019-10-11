9ddddcc27a8ef6c9fed4b0e0f23f127ddb6cc611
Jose Orellana <38052329+jose98orellana@users.noreply.github.com>
2019-10-11 16:08:58 -0600

    Rename Method
    Refactoring #1

--- refactoredA1.java
+++ refactoredA1.java
@@ -10,30 +10,56 @@ This is a hospital management program
 
 class Doctor {
 
-	private String firstName;
-	private String lastName;
+	public String firstName;
+	public String lastName;
 
-	public void func1(String value) {
+	public Doctor(String first, String last) {
+		this.firstName = first;
+		this.lastName = last;
+	}
+
+	public void setFirstName(String value) {
 		firstName = value;
 	}
-	public void func2(String value) {
+	public void setLastName(String value) {
 		lastName = value;
 	}
 }
 
 class Specialist extends Doctor {
 
-	private String type;
-	private double erHours;
-	private double surgeryHours;
+	public String type;
+	public double erHours;
+	public double surgeryHours;
+
+	public Specialist(String first, String last, String newType){
+		super(first, last);
+		this.type = newType;
+	}
 
 	public String getLastName() {
 		return lastName;
 	}
 
+	public void setType(String newType) {
+		type = newType;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setERHours(double addHours) {
+		erHours = erHours + addHours;
+	}
+
+	public void setSurgeryHours(double addHours) {
+		surgeryHours = surgeryHours + addHours;
+	}
+
 	public double calculateWage(int daysWorked) {
-		double erWage = erHours * 40
-		double surgeryWage = surgeryHours * 50
+		double erWage = erHours * 40;
+		double surgeryWage = surgeryHours * 50;
 		return erWage + surgeryWage;
 	}
 
@@ -41,36 +67,64 @@ class Specialist extends Doctor {
 
 class Resident extends Doctor {
 
-	private double erHours;
-	private double surgeryHours;
+	public double erHours;
+	public double surgeryHours;
+
+	public Resident(String first, String last){
+		super(first, last);
+	}
 
 	public String getLastName() {
 		return lastName;
 	}
 
+	public void setERHours(double addHours) {
+		erHours = erHours + addHours;
+	}
+
+	public void setSurgeryHours(double addHours) {
+		surgeryHours = surgeryHours + addHours;
+	}
+
 	public double calculateWage(int daysWorked) {
-		double erWage = erHours * 25
-		double surgeryWage = surgeryHours * 30
+		double erWage = erHours * 25;
+		double surgeryWage = surgeryHours * 30;
 		return erWage + surgeryWage;
 	}
 }
 
 class Surgeon extends Specialist {
 
-	public double getSurgeryCost() {
- 		return calculateWage();
+	public Surgeon(String first, String last, String type){
+		super(first, last, type);
+	}
+
+	public double getSurgeonCost() {
+ 		if (type.equals("senior")) {
+ 			return 2000;
+ 		} else if (type.equals("rookie")) {
+ 			return 500;
+ 		} else {
+ 			return 1000;
+ 		}
  	}
 }
 
 class Surgery {
 
-	private String type;
-	private int roomNumber;
+	public String type;
+	public Surgeon s;
+	public int roomNumber;
+
+	public Surgery(Surgeon surgeon, String newType) {
+		this.s = surgeon;
+		this.type = newType;
+	}
 
  	public double getSurgeryCost() {
- 		if (type.equalsTo("complicated")) {
+ 		if (type.equals("complicated")) {
  			return 2000;
- 		} else if (type.equalsTo("simple")) {
+ 		} else if (type.equals("simple")) {
  			return 500;
  		} else {
  			return 1000;
@@ -80,31 +134,48 @@ class Surgery {
 
 class Patient {
 
-	private String firstName;
-	private String lastName;
+	public String firstName;
+	public String lastName;
+	public Surgery surgery;
 
-	public void func1(String value) {
+	public Patient(String first, String last, Surgery c) {
+		this.firstName = first;
+		this.lastName = last;
+		this.surgery = c;
+	}
+
+	public void setFirstName(String value) {
 		firstName = value;
 	}
-	public void func2(String value) {
+	public void setLastName(String value) {
 		lastName = value;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
 	}
 }
 
 class Bill {
 
-	public double calculateBill(Patient patient) {
-		double surgeryFee = getSurgeryCost();
-		double surgeonFee = getSurgeonCost();
+	public static double calculateBill(Patient patient) {
+		double surgeryFee = patient.surgery.getSurgeryCost();
+		double surgeonFee = patient.surgery.s.getSurgeonCost();
 		double roomFee = 500;
 		double hospitalFee = 300;
 		return surgeryFee + surgeonFee + roomFee + hospitalFee;
 	}
 }
 
-public class Hospital {
+public class A1 {
 	public static void main(String args[]) {
-  		Surgeon s = new Surgeon();
-  		Patient p = new Patient();
+  		Surgeon s = new Surgeon("Jane", "Doe", "senior");
+  		Surgery c = new Surgery(s, "complicated");
+  		Patient p = new Patient("John", "Smith", c);
+
+  		System.out.println(p.getFirstName() + " " + p.getLastName() + " bill is $" + Bill.calculateBill(p));
  	}
 }
